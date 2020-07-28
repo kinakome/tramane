c9c9f5478e4753252a56cdef8881ecabbae25e6e
kinakome <ryomatsu0120@gmail.com>
2020-07-28 21:20:55 +0900

    [Update] Secret Key変更

--- tramane/settings.py
+++ tramane/settings.py
@@ -19,8 +19,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%*jz9!mooxu0ohbtt$=u5+#080%84#1u4$ul12^mvcr+txmb!p'
+# secretをlocal setting に読みにいくようにする
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
